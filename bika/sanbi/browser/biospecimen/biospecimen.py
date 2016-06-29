ce97a188792dd753754caf0884e81854936ed59d
hocinebendou <hocinebendou@gmail.com>
2016-06-29 11:25:21 +0200

    Biospecimen linked to sample storage

--- bika/sanbi/browser/biospecimen/biospecimen.py
+++ bika/sanbi/browser/biospecimen/biospecimen.py
@@ -1,9 +1,82 @@
-from Products.CMFPlone.utils import safe_unicode
 from bika.lims import bikaMessageFactory as _
 from plone.app.content.browser.interfaces import IFolderContentsView
 from plone.app.layout.globals.interfaces import IViewView
 from zope.interface import implements
 from bika.sanbi.browser.multimage import MultimagesView
+from bika.lims.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.ATContentTypes.lib import constraintypes
+from Products.CMFCore.utils import getToolByName
+import json
+
+class BiospecimenEdit(BrowserView):
+    template = ViewPageTemplateFile('templates/biospecimen_edit.pt')
+    title = _("Biospecimen Add/Edit")
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+        super(BiospecimenEdit, self).__init__(context, request)
+        self.icon = self.portal_url + \
+                    "/++resource++bika.sanbi.images/biospecimen_big.png"
+
+    def __call__(self):
+        portal = self.portal
+        form = self.request.form
+
+        if 'submit' in self.request:
+            self.context.setConstrainTypesMode(constraintypes.DISABLED)
+
+            portal_factory = getToolByName(self.context, 'portal_factory')
+            biospecimen = portal_factory.doCreate(self.context, self.context.id)
+            biospecimen.processForm()
+            biospecimen.edit(
+                quantity=1
+            )
+            obj_url = biospecimen.absolute_url_path()
+            self.request.response.redirect(obj_url)
+            return
+
+        return self.template()
+
+    def get_fields_with_visibility(self, visibility, mode=None):
+        mode = mode if mode else 'edit'
+        schema = self.context.Schema()
+        fields = []
+        for field in schema.fields():
+            isVisible = field.widget.isVisible
+            v = isVisible(self.context, mode, default='invisible', field=field)
+            if v == visibility:
+                fields.append(field)
+        return fields
+
+    def get_storage_units(self):
+        display_list = list(self.context.getStorageUnits().items())
+        return display_list
+
+class AjaxBoxPositions:
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        form = self.request.form
+        uc = getToolByName(self.context, 'uid_catalog')
+        bsc = getToolByName(self.context, 'bika_setup_catalog')
+        brains = uc(UID=form['uid'])
+        box = brains[0].getObject()
+        brains = brains = bsc.searchResults(
+                        portal_type='StorageLocation',
+                        inactive_state='active',
+                        sort_on='sortable_title',
+                        path={'query': "/".join(box.getPhysicalPath()), 'level': 0})
+
+        message = 'No free position in stock. Please select another container.' if not len(brains) else ''
+        if message:
+            return json.dumps({'error': message})
+
+        return json.dumps({'uid': brains[0].UID, 'address': brains[0].title})
 
 class BiospecimenMultimageView(MultimagesView):
     implements(IFolderContentsView, IViewView)
--- bika/sanbi/content/biospecimen.py
+++ bika/sanbi/content/biospecimen.py
@@ -4,14 +4,14 @@ from Products.Archetypes.atapi import *
 from plone.indexer import indexer
 from zope.interface import implements
 from bika.lims.content.bikaschema import BikaSchema, BikaFolderSchema
-from bika.lims.browser.widgets import DateTimeWidget as bika_DateTimeWidget
 from plone.app.folder.folder import ATFolder
 from bika.sanbi import bikaMessageFactory as _
 from bika.sanbi.interfaces import IBiospecimen
 from bika.sanbi.config import PROJECTNAME
 from Products.CMFCore.utils import getToolByName
 from DateTime import DateTime
-from plone.formwidget.datetime.at import DatetimeWidget
+from bika.lims.browser.widgets import ReferenceWidget as bika_ReferenceWidget
+
 
 schema = BikaFolderSchema.copy() + BikaSchema.copy() + Schema((
     ReferenceField('Type',
@@ -30,20 +30,17 @@ schema = BikaFolderSchema.copy() + BikaSchema.copy() + Schema((
         widget=StringWidget(
             label=_("Specimen condition"),
             description=_("A specimen condition is a 3-letter code that indicates the status of a specimen. "
-                          "Eg, SAT for satisfactory.")
+                          "Eg, SAT for satisfactory."),
+            visible={'edit': 'visible', 'view': 'visible'}
         )),
 
     StringField('SubjectID',
         searchable=True,
         widget=StringWidget(
             label=_("Subject ID"),
-            description=_("Human-subject ID the specimen is taken from.")
+            description=_("Human-subject ID the specimen is taken from."),
+            visible={'edit': 'visible', 'view': 'visible'}
         )),
-
-    # StringField('Volume',
-    #     widget=StringWidget(
-    #         label=_("Volume"),
-    #     )),
 
     FixedPointField('Volume',
         required=1,
@@ -52,36 +49,109 @@ schema = BikaFolderSchema.copy() + BikaSchema.copy() + Schema((
             label=_("Volume"),
             size=15,
             description=_("The The volume of the biospecimen taken from the subject."),
+            visible={'edit': 'visible', 'view': 'visible'}
         )),
 
     StringField('Unit',
         widget=StringWidget(
             label=_("Unit"),
+            visible={'edit': 'visible', 'view': 'visible'}
         )),
-
-    # DateTimeField('dateReceived',
-    #     searchable=1,
-    #     widget=bika_DateTimeWidget(
-    #         label='Date Received'
-    #     )),
-
-    DateTimeField('datetimeReceived',
-          default_method=DateTime,
-          widget=CalendarWidget(
-              label='Date and Time Received',
-              description='Select the date and time the biospecimen is received.',
-              ampm=1,
-          )),
 
     ComputedField('VolumeUsed',
           expression='context.getVolumeUsed()',
           widget=ComputedWidget(
-              label=_("VAT"),
-              visible={'edit': 'hidden',}
-          ))
+              label=_("Volume Used"),
+              visible={'edit': 'invisible', 'view': 'invisible'}
+          )),
+
+    ReferenceField('StorageUnits',
+        vocabulary='getStorageUnits',
+        allowed_types=('StorageUnit',),
+        relationship='AliquotUnit',
+        widget=SelectionWidget(
+            format='select',
+            label=_("Storage Units"),
+            visible={'view': 'invisible', 'edit': 'invisible'}
+        )),
+
+    ReferenceField('Freezers',
+        vocabulary='getFreezers',
+        allowed_types=('StorageManagement',),
+        relationship='AliquotStorage',
+        widget=SelectionWidget(
+            format='select',
+            label=_("Freezers"),
+            visible={'view': 'invisible', 'edit': 'invisible'}
+        )),
+
+    ReferenceField('Shelves',
+        vocabulary='getShelves',
+        allowed_types=('StorageManagement',),
+        relationship='AliquotStorage',
+        widget=SelectionWidget(
+            format='select',
+            label=_("Shelves"),
+            visible={'view': 'invisible', 'edit': 'invisible'}
+        )),
+
+    ReferenceField('Boxes',
+        vocabulary='getBoxes',
+        allowed_types=('StorageManagement',),
+        relationship='AliquotStorage',
+        widget=SelectionWidget(
+            format='select',
+            label=_("Boxes"),
+            visible={'view': 'invisible', 'edit': 'invisible'}
+        )),
+
+    ReferenceField(
+        'StorageLocation',
+        allowed_types=('StorageLocation',),
+        relationship='AliquotStorageLocation',
+        widget=bika_ReferenceWidget(
+            label=_("Storage Location"),
+            description=_("Location where sample is kept"),
+            size=40,
+            visible={'edit': 'visible', 'view': 'visible'},
+            catalog_name='bika_setup_catalog',
+            showOn=True,
+            render_own_label=True,
+            base_query={'inactive_state': 'active', 'review_state': 'position_free'},
+            colModel=[{'columnName': 'UID', 'hidden': True},
+                      {'columnName': 'Room', 'width': '15', 'label': _('Room')},
+                      {'columnName': 'StorageType', 'width': '15', 'label': _('Type')},
+                      {'columnName': 'Shelf', 'width': '13', 'label': _('Sh./Ca.')},
+                      {'columnName': 'Box', 'width': '13', 'label': _('Box/Cane')},
+                      {'columnName': 'Position', 'width': '13', 'label': _('Pos.')},
+                      {'columnName': 'Title', 'width': '31', 'label': _('Address')},
+                      ],
+        )),
+
+    DateTimeField(
+        'DatetimeReceived',
+        default_method=DateTime,
+        widget=CalendarWidget(
+            label='Date and Time Received',
+            description='Select the date and time the biospecimen is received.',
+            ampm=1,
+            visible={'edit': 'visible', 'view': 'visible'}
+        )),
+
+    IntegerField(
+        'Quantity',
+        required=1,
+        default=1,
+        widget=IntegerWidget(
+            label=_("Quantity"),
+            size=15,
+            description=_("The number of units that this sample represents."),
+            visible={'edit': 'invisible', 'view': 'invisible'}
+        )),
 ))
 
-schema['description'].widget.visible = True
+schema['title'].widget.visible = {'edit': 'visible', 'view': 'visible'}
+schema['description'].widget.visible = {'edit': 'visible', 'view': 'visible'}
 schema['description'].schemata = 'default'
 
 
@@ -129,6 +199,49 @@ class Biospecimen(ATFolder):
             total_volume += float(quantity * volume)
 
         return total_volume
+
+    def getStorageUnits(self):
+        bsc = getToolByName(self, 'bika_setup_catalog')
+        items = [(c.UID, c.Title)
+                 for c in bsc(portal_type='StorageUnit',
+                              inactive_state='active')]
+        items.sort(lambda x, y: cmp(x[1], y[1]))
+        return DisplayList(items)
+
+    def at_post_create_script(self):
+        """Execute once the project is created
+        """
+        wftool = self.portal_workflow
+        location = self.getStorageLocation()
+        if location:
+            wftool.doActionFor(location, action='reserve', wf_id='bika_storageposition_workflow')
+            location.edit(
+                IsReserved=True,
+                IsOccupied=False
+            )
+            location.setSample(self)
+            location.reindexObject()
+
+    def workflow_script_receive(self):
+        self.setDatetimeReceived(DateTime())
+
+    def workflow_script_store(self):
+        """
+        """
+        wftool = self.portal_workflow
+        storage_location = self.getStorageLocation()
+        if storage_location:
+            state = wftool.getInfoFor(storage_location, 'review_state')
+            if state == 'position_reserved':
+                if not storage_location.getSample():
+                    storage_location.setSample(self)
+
+                storage_location.edit(
+                    IsReserved=False,
+                    IsOccupied=True
+                )
+                storage_location.reindexObject()
+                wftool.doActionFor(storage_location, action='occupy', wf_id="bika_storageposition_workflow")
 
 schemata.finalizeATCTSchema(schema, folderish = True, moveDiscussion = False)
 registerType(Biospecimen, PROJECTNAME)
