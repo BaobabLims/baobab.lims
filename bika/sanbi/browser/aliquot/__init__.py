bdd204d6af5301c88dd95ca92f65c16be111b9ce
hocinebendou <hocinebendou@gmail.com>
2016-09-04 07:47:28 +0200

    cleanup aliquot edit template

--- bika/sanbi/browser/aliquot/__init__.py
+++ bika/sanbi/browser/aliquot/__init__.py
@@ -1,7 +1,2 @@
-from aliquot import AjaxGetBiospecimenVolume
-from aliquot import AjaxGetBoxes
-from aliquot import AjaxGetFreezers
-from aliquot import AjaxGetPositions
-from aliquot import AjaxGetShelves
 from aliquot import AliquotEdit
 
