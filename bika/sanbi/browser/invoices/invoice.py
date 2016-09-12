7269d40492a43cb7801f80a169225b7394e1e116
hocinebendou <hocinebendou@gmail.com>
2016-09-12 09:25:34 +0200

    kit invoicing added.

--- bika/sanbi/browser/invoices/invoice.py
+++ bika/sanbi/browser/invoices/invoice.py
@@ -1,6 +1,11 @@
 from zope.interface.declarations import implements
+from Products.Five import BrowserView
 from plone.app.layout.viewlets import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.schema import ValidationError
+from plone import api
+
+from bika.lims.idserver import renameAfterCreation
 from bika.sanbi.config import INVOICE_SERVICES
 
 
@@ -14,3 +19,56 @@ class AddInvoiceViewlet(ViewletBase):
 
     def render(self):
         return self.index()
+
+class AddInvoiceSubmitHandler(BrowserView):
+    """Handle submit information from invoice viewlet
+    """
+
+    def __call__(self):
+
+        if "viewlet_submitted" in self.request.form:
+            data = {}
+            try:
+                data = self.validate_form_inputs()
+            except ValidationError as e:
+                self.form_error(e.message)
+
+            obj = api.content.create(
+                container=self.context,
+                title=data['title'],
+                type='InvoiceBatch',
+                Project=data['project_uid'],
+                Service=data['service'],
+                BatchStartDate=data['start_date'],
+                BatchEndDate=data['end_date']
+            )
+            renameAfterCreation(obj)
+            obj.reindexObject()
+
+            msg = u'Invoice for period "%s" to "%s" created.' % (data['start_date'], data['end_date'])
+            self.context.plone_utils.addPortalMessage(msg)
+            self.request.response.redirect(self.context.absolute_url())
+
+    def validate_form_inputs(self):
+        """Validate form inputs
+        """
+        form = self.request.form
+        if not form['invoicetitle']:
+            raise ValidationError('Title should be specified for this invoice!')
+        if not form['invoice_service']:
+            raise ValidationError('Invoice service is required!')
+        if form['startdate'] and form['enddate']:
+            if form['enddate'] < form['startdate']:
+                raise ValidationError(u"Start date, '%s', shouldn't be greater than End date, '%s'!" %(form['startdate'], form['enddate']))
+
+        return {
+            'title': form['invoicetitle'],
+            'project_uid': form['Project_uid'],
+            'service': form['invoice_service'],
+            'start_date': form['startdate'],
+            'end_date': form['enddate']
+        }
+
+    def form_error(self, msg):
+        self.context.plone_utils.addPortalMessage(msg)
+        self.request.response.redirect(self.context.absolute_url())
--- bika/sanbi/extenders/invoicebatch.py
+++ bika/sanbi/extenders/invoicebatch.py
@@ -1,15 +1,22 @@
 from archetypes.schemaextender.interfaces import IOrderableSchemaExtender
 from zope.interface import implements
 from zope.component import adapts
-from Products.CMFCore.utils import getToolByName
 from Products.Archetypes.references import HoldingReference
 from Products.Archetypes.atapi import SelectionWidget
+from AccessControl import ClassSecurityInfo
+from DateTime import DateTime
+from Products.CMFPlone.utils import _createObjectByType
+from zope.container.contained import ContainerModifiedEvent
 
+from bika.lims.content.invoice import InvoiceLineItem
 from bika.lims.fields import ExtReferenceField, ExtStringField
 from bika.lims.interfaces import IInvoiceBatch
 from bika.lims.browser.widgets import ReferenceWidget as bika_ReferenceWidget
+from bika.lims.config import ManageInvoices
+from bika.lims.content.invoicebatch import InvoiceBatch
 from bika.sanbi import bikaMessageFactory as _
 from bika.sanbi.config import INVOICE_SERVICES
+
 
 class InvoiceBatchSchemaExtender(object):
     adapts(IInvoiceBatch)
@@ -55,3 +62,90 @@ class InvoiceBatchSchemaExtender(object):
         sch.insert(sch.index('BatchStartDate'), 'Service')
         schematas['default'] = sch
         return schematas
+
+
+class Invoicing(InvoiceBatch):
+    """Class for creating invoices.
+    """
+
+    security = ClassSecurityInfo()
+
+    def __init__(self, instance, project, service, brains):
+        super(Invoicing, self).__init__(self)
+        self.instance = instance
+        self.project = project
+        self.service = service
+        self.brains = brains
+        self.client_uid = self.project.getClient().UID()
+
+    security.declareProtected(ManageInvoices, 'create_invoice')
+
+    def create_invoice(self):
+        invoice_id = self.instance.generateUniqueId('Invoice')
+        invoice = _createObjectByType("Invoice", self.instance, invoice_id)
+        invoice.edit(
+            Client=self.client_uid,
+            InvoiceDate=DateTime(),
+        )
+        invoice.processForm()
+        invoice.invoice_lineitems = []
+        start = self.instance.getBatchStartDate()
+        end = self.instance.getBatchEndDate()
+        if self.service == 'Kit':
+            sub_total, vat = 0, 0
+            for brain in self.brains:
+                kit = brain.getObject()
+                kit_template = kit.getKitTemplate()
+                sub_total += float(kit_template.getPrice())
+                vat = float(kit_template.getVAT())
+            total = sub_total + (sub_total * vat / 100)
+
+            lineitem = InvoiceLineItem()
+            lineitem['ItemDate'] = self.project.getDateCreated()
+            lineitem['OrderNumber'] = self.project.getId()
+            lineitem['AnalysisRequest'] = ''
+            lineitem['SupplyOrder'] = ''
+            lineitem['Project'] = self.project
+            lineitem['ItemDescription'] = 'Kit invoicing for the period {} to {}'.format(start.strftime('%Y-%m-%d'),
+                                                                                         end.strftime('%Y-%m-%d'))
+            lineitem['Subtotal'] = sub_total
+            lineitem['VATAmount'] = vat
+            lineitem['Total'] = total
+            invoice.invoice_lineitems.append(lineitem)
+        invoice.reindexObject()
+        return invoice
+
+def ObjectModifiedEventHandler(instance, event):
+    """Create kit and storage invoices
+    """
+    if not isinstance(event, ContainerModifiedEvent):
+        start = instance.getBatchStartDate()
+        end = instance.getBatchEndDate()
+        field = instance.getField('Service')
+        service = field.getAccessor(instance)()
+        field = instance.getField('Project')
+        project = field.getAccessor(instance)()
+
+        # field.getMutator(instance)('Storage')
+
+        # Query for kits in date range
+        brains = []
+        if service == 'Kit':
+            query = {
+                'portal_type': 'Kit',
+                'inactive_state': 'active',
+                'kit_project_uid': project.UID(),
+                'getDateCreated': {
+                    'range': 'min:max',
+                    'query': [start, end]
+                }
+            }
+
+            brains = instance.bika_catalog(query)
+            # for k in kits:
+            #     kit = k.getObject()
+            #     kit_template = kit.getKitTemplate()
+            #     cost = kit_template.getCost()
+
+        invoicing = Invoicing(instance, project, service, brains)
+        invoicing.create_invoice()
