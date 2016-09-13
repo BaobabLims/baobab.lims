a75a0e8d17f76e5f0067380497e4ede5ee290794
hocinebendou <hocinebendou@gmail.com>
2016-09-13 10:52:48 +0200

    get the list of products select in a kit template using uids.

--- bika/sanbi/browser/ajax/getproducts.py
+++ bika/sanbi/browser/ajax/getproducts.py
@@ -55,8 +55,8 @@ class ajaxGetProducts(BrowserView):
 
 
 class ComputeTotalPrice(BrowserView):
-    """This class is used to compute the total price for kit-template 
-    products."""
+    """Return prices of products selected in kit template.
+    """
 
     def __init__(self, context, request):
         super(ComputeTotalPrice, self).__init__(context, request)
@@ -64,13 +64,15 @@ class ComputeTotalPrice(BrowserView):
         self.request = request
 
     def __call__(self):
-        titles = self.request.form['titles[]']
-        catalog = 'bika_setup_catalog'
-        bsc = getToolByName(self.context, catalog)
-        brains = bsc.searchResults(portal_type='Product', title=titles)
-
         ret = []
-        for brain in brains:
-            ret.append({'title': brain.title, 'price': brain.getPrice})
+        if 'uids[]' in self.request.form:
+            uids = self.request.form['uids[]']
+            catalog = 'uid_catalog'
+            uc = getToolByName(self.context, catalog)
+            brains = uc.searchResults(UID=uids)
+
+            for brain in brains:
+                product = brain.getObject()
+                ret.append({'title': product.Title(), 'price': product.getPrice()})
 
         return json.dumps(ret)
