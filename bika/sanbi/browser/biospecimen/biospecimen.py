0cac870a6752db70d5689b85090fd8106cd39795
hocinebendou <hocinebendou@gmail.com>
2016-10-04 12:26:10 +0200

    cleaning up

--- bika/sanbi/browser/biospecimen/biospecimen.py
+++ bika/sanbi/browser/biospecimen/biospecimen.py
@@ -1,63 +1,13 @@
 import json
 
-from Products.ATContentTypes.lib import constraintypes
 from Products.CMFCore.utils import getToolByName
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.content.browser.interfaces import IFolderContentsView
 from plone.app.layout.globals.interfaces import IViewView
 from zope.interface import implements
 
 from bika.lims import bikaMessageFactory as _
-from bika.lims.browser import BrowserView
 from bika.sanbi.browser.multimage import MultimagesView
 from bika.sanbi.interfaces import IBioSpecimenStorage
-
-
-class BiospecimenEdit(BrowserView):
-    template = ViewPageTemplateFile('templates/biospecimen_edit.pt')
-    title = _("Biospecimen Add/Edit")
-
-    def __init__(self, context, request):
-        super(BiospecimenEdit, self).__init__(context, request)
-        self.context = context
-        self.request = request
-
-        self.icon = self.portal_url + \
-                    "/++resource++bika.sanbi.images/biospecimen_big.png"
-
-    def __call__(self):
-        portal = self.portal
-        form = self.request.form
-
-        if 'submit' in self.request:
-            self.context.setConstrainTypesMode(constraintypes.DISABLED)
-
-            portal_factory = getToolByName(self.context, 'portal_factory')
-            biospecimen = portal_factory.doCreate(self.context, self.context.id)
-            biospecimen.processForm()
-            biospecimen.edit(
-                quantity=1
-            )
-            obj_url = biospecimen.absolute_url_path()
-            self.request.response.redirect(obj_url)
-            return
-
-        return self.template()
-
-    def get_fields_with_visibility(self, visibility, mode=None):
-        mode = mode if mode else 'edit'
-        schema = self.context.Schema()
-        fields = []
-        for field in schema.fields():
-            isVisible = field.widget.isVisible
-            v = isVisible(self.context, mode, default='invisible', field=field)
-            if v == visibility:
-                fields.append(field)
-        return fields
-
-    def get_storage_units(self):
-        display_list = list(self.context.getStorageUnits().items())
-        return display_list
 
 
 class AjaxBoxPositions:
