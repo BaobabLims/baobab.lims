955fa318a6844ab4e17bf49165f297a9f212429b
hocinebendou <hocinebendou@gmail.com>
2016-11-24 00:37:38 +0200

    remove labproduct from invoicing. for now.

--- bika/sanbi/config.py
+++ bika/sanbi/config.py
@@ -34,6 +34,5 @@ PRICELIST_TYPES = DisplayList((
 INVOICE_SERVICES = DisplayList((
     ('Kit', _('Kit')),
     ('Storage', _('Sample Storage')),
-    ('LabProduct', _('Lab Product')),
     ('AnalysisRequest', _('Analysis Request')),
 ))
