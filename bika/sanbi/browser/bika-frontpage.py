2b993d48ea6e6c16d097f5a2e8fe995f78185bd5
hocinebendou <hocinebendou@gmail.com>
2017-01-21 11:51:28 +0200

    Override default front-page

--- bika/sanbi/browser/bika-frontpage.py
+++ bika/sanbi/browser/bika-frontpage.py
@@ -1,7 +1,7 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api as ploneapi
 
 from bika.lims.browser import BrowserView
-
 
 class FrontPageView(BrowserView):
     template = ViewPageTemplateFile("bika-frontpage.pt")
@@ -10,6 +10,42 @@ class FrontPageView(BrowserView):
         self.set_versions()
         self.icon = self.portal_url + \
                     "/++resource++bika.lims.images/chevron_big.png"
+
+        bika_setup = ploneapi.portal.get_tool("bika_setup")
+        landingpage = bika_setup.getLandingPage()
+        if self.is_anonymous_user():
+            # Redirect to the selected Landing Page
+            if landingpage:
+                return self.request.response.redirect(landingpage.absolute_url())
+            # Show the Bika Front Page
+            return self.template()
+
+        # Authenticated Users get either the Dashboard, the std. Bika Frontpage
+        # or the custom landing page. Furthermore, they can switch between the
+        # Dashboard and the landing page.
+
+        # First precedence: Request parameter `redirect_to`
+        redirect_to = self.request.form.get("redirect_to", None)
+        if redirect_to == "dashboard":
+            return self.request.response.redirect(self.portal_url + "/bika-dashboard")
+        if redirect_to == "frontpage":
+            if landingpage:
+                return self.request.response.redirect(landingpage.absolute_url())
+            return self.template()
+
+        # Second precedence: Dashboard enabled
+        if self.is_dashboard_enabled():
+            roles = self.get_user_roles()
+            if 'Manager' in roles or 'LabManager' in roles:
+                return self.request.response.redirect(self.portal_url + "/bika-dashboard")
+            if 'Sampler' in roles or 'SampleCoordinator' in roles:
+                return self.request.response.redirect(
+                    self.portal_url + "/samples?samples_review_state=to_be_sampled")
+
+        # Third precedence: Custom Landing Page
+        if landingpage:
+            return self.request.response.redirect(landingpage.absolute_url())
+
         return self.template()
 
     def set_versions(self):
@@ -24,3 +60,15 @@ class FrontPageView(BrowserView):
             info = qi.upgradeInfo(key)
             if info and 'installedVersion' in info:
                 self.upgrades[key] = info['installedVersion']
+
+    def is_dashboard_enabled(self):
+        """Checks if the dashboard is enabled
+        """
+        bika_setup = ploneapi.portal.get_tool("bika_setup")
+        return bika_setup.getDashboardByDefault()
+
+    def is_anonymous_user(self):
+        """Checks if the current user is anonymous
+        """
+        return ploneapi.user.is_anonymous()
+
