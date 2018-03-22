13f7d42df930a14e09ce10ed95ff79729eaec38e
hocinebendou <hocinebendou@gmail.com>
2018-03-22 15:21:46 +0200

    Add Project type to Allowed types for api creation

--- baobab/lims/jsonapi/config.py
+++ baobab/lims/jsonapi/config.py
@@ -3,7 +3,8 @@ from bika.lims.jsonapi import config
 ALLOWED_PORTAL_TYPES_TO_CREATE = {
     'StorageUnit',
     'ManagedStorage',
-    'UnmanagedStorage'
+    'UnmanagedStorage',
+    'Project'
 }
 
 ALLOWED_PORTAL_TYPES_TO_CREATE.update(config.ALLOWED_PORTAL_TYPES_TO_CREATE)
