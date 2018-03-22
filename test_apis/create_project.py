612a72ea888334609afb3eeb769ea7a953cbd8a6
hocinebendou <hocinebendou@gmail.com>
2018-03-22 15:20:50 +0200

    Update project api creation script

--- test_apis/create_project.py
+++ test_apis/create_project.py
@@ -8,7 +8,20 @@ password = 'hocine'
 auth = HTTPBasicAuth(username, password)
 url = top_level_url + '/@@API/v3/create'
 
-data = {"BODY": '[{"portal_type": "Project", "parent_path": "clients/client-1", "StudyType": "eclectic", "title": "White Goblin lvl 1", "description": "Cleaning up the side effects of Goblin serum", "NumParticipants": "412", "AgeHigh": "24", "AgeLow": "4", "DateCreated": "2018-02-22", "Service": ["400d67e0f61d4487a71f2174d4eed3f6","1c9fc9da29ca4d049dc74484cddd7799"]}]'}
+data = {"BODY": '[{ \
+        "portal_type": "Project", \
+        "parent_path": "clients/client-1", \
+        "StudyType": "eclectic", \
+        "title": "White Goblin lvl 1", \
+        "description": "Cleaning up the side effects of Goblin serum", \
+        "NumParticipants": "412", \
+        "AgeHigh": "24", \
+        "AgeLow": "4", \
+        "DateCreated": "2018-02-22", \
+        "SampleType": ["Blood Plasma", "Serum"], \
+        "Service": ["DNA concentration","Mass Spectrometry"] \
+        }]'
+}
 
 request = requests.post(url, data=data, auth=auth)
 # show the response content
