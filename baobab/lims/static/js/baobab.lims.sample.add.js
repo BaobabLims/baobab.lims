c876d2bebdf96f549d5a16579d21bfa12d8682db
coertquinton <coertquinton@gmail.com>
2018-11-23 15:41:15 +0200

    Add code to display date and time for frozen time and sampling date

--- baobab/lims/static/js/baobab.lims.sample.add.js
+++ baobab/lims/static/js/baobab.lims.sample.add.js
@@ -13,7 +13,7 @@ function BaobabSampleView() {
         });
 
         $('tr[fieldname=Kit]').hide();
-        // $('tr[fieldname=BabyNumber]').hide();
+        $('tr[fieldname=BabyNumber]').hide();
         sampleTypeSelected($('#SampleType_uid').val());
 
         $('#SampleType_uid').focus(function() {
@@ -49,37 +49,34 @@ function BaobabSampleView() {
             }
         });
 
-        // var requestData = {
-        //     catalog_name: "portal_catalog",
-        //     portal_type: "Sample",
-        //     UID: $('#LinkedSample_uid').val()
-        // };
-        // window.bika.lims.jsonapi_read(requestData, function (data) {
-        //     if (data.success && data.total_objects > 0) {
-        //         console.log('=====10---------')
-        //         console.log(data.objects[0])
-        //
-        //         var creation_date = data.objects[0]['creation_date']
-        //         var cfg_date_time = data.objects[0]['CfgDateTime']
-        //
-        //         var final_creation_date = getDatePickerDateAndTime(creation_date)
-        //         var final_cfg_date_time = getDatePickerDateAndTime(cfg_date_time)
-        //
-        //         $('#DateCreated').val(final_creation_date)
-        //         $('#CfgDateTime').val(final_cfg_date_time)
-        //
-        //     }
-        // });
+        // console.log('========================')
+        var path = window.location.href.split('edit')[0] + '/get_sample_dates';
+        // console.log(path)
+        var title = $('#breadcrumbs-current').text()
+        // console.log(title)
 
         // var path = window.location.href.split('/base_view')[0] + '/retrieve_date_times';
-        // $.ajax({
-        //     type: 'GET',
-        //     dataType: 'json',
-        //     url: path,
-        //     data: {'locTitle': $('#StorageLocation').val()}
-        // }).done(function (data) {
-        //     console.log(data);
-        // })
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: path,
+            data: {'title': title}
+        }).done(function (data) {
+            // console.log('-----------')
+            // console.log(data);
+
+            var sampling_date = data['sampling_date']
+            if (sampling_date) {
+                var final_sampling_date = getDatePickerDateAndTime(sampling_date)
+                $('#SamplingDate').val(final_sampling_date)
+            }
+
+            var frozen_time = data['frozen_time']
+            if (frozen_time) {
+                var final_frozen_time = getDatePickerDateAndTime(frozen_time)
+                $('#FrozenTime').val(final_frozen_time)
+            }
+        })
 
     };
 
@@ -147,72 +144,28 @@ function BaobabSampleView() {
         });
     }
 
-    function getDatePickerDateAndTime(creation_date){
-        new_date = new Date(creation_date)
+    function getDatePickerDateAndTime(plone_date){
+        try{
+            // console.log('-----------------------')
+            var new_date = new Date(plone_date)
 
-        var gmt_format_date = getGMTFormatDate(creation_date)
+            var gmt_format_date = getGMTFormatDate(plone_date)
+            // console.log(gmt_format_date)
 
-        var new_date = new Date(gmt_format_date)
-        final_date_and_time = getDateString(new_date)
+            return gmt_format_date  //final_date_and_time
+        }
+        catch(err){
+            console.log("Error: " + err.message)
+            return plone_date
+        }
 
-        return final_date_and_time
     }
 
     function getGMTFormatDate(plone_date_string){
-        if (plone_date_string.includes('GMT')){
-            gmt_split = plone_date_string.split('GMT')
-            date_part = gmt_split[0].trim()
-            gmt_part = gmt_split[1]
-        } else {
-            gmt_part = null
-            date_part = plone_date_string
-        }
 
-        var pieces = date_part.split(/[-/ :]/)
-        var month_string = getMonthString(pieces[1])
+        var pieces = plone_date_string.split(/[-/ :]/)
 
-        var gmt_format_date = month_string + ' ' + pieces[2] + ', ' + pieces[0] + ' ' + pieces[3] + ':' + pieces[4]
-        if (gmt_part != null){
-            gmt_format_date = gmt_format_date + ' GMT' + gmt_part
-        }
-
-        return gmt_format_date
-    }
-
-    function getMonthString(month_number){
-        var months = {
-                    '01': 'January',
-                    '02': 'February',
-                    '03': 'March',
-                    '04': 'April',
-                    '05': 'May',
-                    '06': 'June',
-                    '07': 'July',
-                    '08': 'August',
-                    '09': 'September',
-                    '10': 'October',
-                    '11': 'November',
-                    '12': 'December'
-                };
-
-        return months[month_number]
-    }
-
-    function getDateString(date){
-        var date_portion = [
-          date.getFullYear(),
-          ('0' + (date.getMonth() + 1)).slice(-2),
-          ('0' + date.getDate()).slice(-2)
-        ].join('-');
-
-        var time_portion = [
-            ('0' + (date.getHours() + 1)).slice(-2),
-            ('0' + date.getMinutes()).slice(-2)
-        ].join(':');
-
-        var final_date_and_time = date_portion + ' ' + time_portion
-
-        return final_date_and_time
+        return [pieces[0], pieces[1], pieces[2]].join('-') + ' ' + [pieces[3], pieces[4]].join(':')
     }
 
 }
