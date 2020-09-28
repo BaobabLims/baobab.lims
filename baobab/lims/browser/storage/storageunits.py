e495e5f3aedf75e0eaba7fb5d8a3fe05cae6d5ca
Lunga Baliwe <lunga001@gmail.com>
2020-09-29 00:18:49 +0200

    Add Goto Link/button on folderish jstree types

--- baobab/lims/browser/storage/storageunits.py
+++ baobab/lims/browser/storage/storageunits.py
@@ -34,7 +34,7 @@ class StorageUnitsView(BikaListingView):
         self.title = self.context.Title()
         self.description = ""
         self.icon = self.portal_url + \
-                    '/++resource++baobab.lims.images/storageunit_big.png'
+            '/++resource++baobab.lims.images/storageunit_big.png'
         self.show_sort_column = False
         self.show_select_row = False
         self.show_select_column = True
@@ -161,7 +161,11 @@ class StorageUnitsView(BikaListingView):
                 ]
             }
         ]
+
     def __call__(self):
+        self.items = BikaListingView.folderitems(self)
+        self.show_workflow_action_buttons = True
+        self.actions = self.get_workflow_actions()
         return self.template()
 
 
@@ -179,176 +183,47 @@ class StorageUnitsxView(BikaListingView):
         self.context = context
         self.request = request
 
-        # self.catalog = 'bika_setup_catalog'
-        path = '/'.join(self.context.getPhysicalPath())
-        self.contentFilter = {
-            'path': {'query': path, 'depth': 1, 'level': 0},
-            'sort_on': 'sortable_title'
-        }
-        self.context_actions = {}
-        self.title = self.context.Title()
-        self.description = ""
-        self.icon = self.portal_url + \
-                    '/++resource++baobab.lims.images/storageunit_big.png'
-        self.show_sort_column = False
-        self.show_select_row = False
-        self.show_select_column = True
-        self.pagesize = 25
-        self.columns = {
-            'Title': {'title': _('Title'),
-                      'index': 'sortable_title'},
-            'Description': {'title': _('Description'),
-                            'toggle': False},
-            'Type': {'title': _('Type')},
-            'Temperature': {'title': _('Temperature'),
-                            'toggle': True},
-            'Department': {'title': _('Department'),
-                           'toggle': True},
-        }
-        self.review_states = [
-            {
-                'id': 'default',
-                'title': _('All'),
-                'contentFilter': {},
-                'columns': [
-                    'Title',
-                    'Type',
-                    'Description',
-                    'Temperature',
-                    'Department'
-                ]
-            },
-            {
-                'id': 'units',
-                'title': _('Storage Units'),
-                'contentFilter': {
-                    'inactive_state': 'active',
-                    'portal_type': 'StorageUnit'
-                },
-                'transitions': [
-                    {'id': 'deactivate'}
-                ],
-                'columns': [
-                    'Title',
-                    'Type',
-                    'Description',
-                    'Temperature',
-                    'Department'
-                ]
-            },
-            {
-                'id': 'managed',
-                'title': _('Managed Storages'),
-                'contentFilter': {
-                    'inactive_state': 'active',
-                    'portal_type': 'ManagedStorage'
-                },
-                'transitions': [
-                    {'id': 'deactivate'},
-                    {'id': 'occupy'}
-                ],
-                'columns': [
-                    'Title',
-                    'Type',
-                    'Description',
-                    'Temperature',
-                    'Department'
-                ]
-            },
-            {
-                'id': 'unmanaged',
-                'title': _('Unmanaged Storages'),
-                'contentFilter': {
-                    'inactive_state': 'active',
-                    'review_state': 'available',
-                    'portal_type': 'UnmanagedStorage'
-                },
-                'transitions': [
-                    {'id': 'deactivate'},
-                    {'id': 'occupy'}
-                ],
-                'columns': [
-                    'Title',
-                    'Type',
-                    'Description',
-                    'Temperature',
-                    'Department'
-                ]
-            },
-            {
-                'id': 'occupied',
-                'title': _('Occupied'),
-                'contentFilter': {
-                    'inactive_state': 'active',
-                    'portal_type': 'UnmanagedStorage',
-                    'review_state': 'occupied'
-                },
-                'transitions': [
-                    {'id': 'deactivate'},
-                    {'id': 'liberate'}
-                ],
-                'columns': [
-                    'Title',
-                    'Type',
-                    'Description',
-                    'Temperature',
-                    'Department'
-                ]
-            },
-            {
-                'id': 'occupied_boxes',
-                'title': _('Occupied Boxes'),
-                'contentFilter': {
-                    'inactive_state': 'active',
-                    'portal_type': 'ManagedStorage',
-                    'review_state': 'occupied'
-                },
-                'transitions': [
-                    {'id': 'deactivate'},
-                    {'id': 'liberate'}
-                ],
-                'columns': [
-                    'Title',
-                    'Type',
-                    'Description',
-                    'Temperature',
-                    'Department'
-                ]
-            }
-        ]
-
-    def __call__(self):
+    def __call__(self, id=None):
         self.positions = self.context.objectValues()
         self.title = self.context.title
-        self.show_select_column = True
-        self.items = BikaListingView.folderitems(self)
-        self.positions_table = [{"id": i.UID(), "text": str(i.title), 'children':True, 'url': i.absolute_url()}
-                for i in self.positions]
+        icon_man = self.portal_url + \
+            '/++resource++baobab.lims.images/storageunit.png'
+        self.positions_table = []
+        for i in self.positions:
+            p_dict = {"id": i.UID(), "text": str(i.title), 'children': True,
+                      'portal_type': i.portal_type}
+            image_url = self.portal_url + '/++resource++baobab.lims.images/'
+            icon_man = image_url + i.portal_type.lower() + '.png'
+            p_dict['icon'] = icon_man
+            print i.title
+            if i.portal_type != 'StoragePosition' and \
+                    i.portal_type != 'UnmanagedStorage':
+                p_dict['url'] = i.absolute_url()
+            p_dict['children'] = True
+            if not i.objectValues():
+                p_dict['children'] = False
+
+            self.positions_table.append(p_dict)
 
         uid = self.request.form.get("id", '#')
         if uid == '#':
             return json.dumps(self.positions_table)
         obj = api.get_object_by_uid(uid)
-        self.positions_table = [{"id": i.UID(), "text": str(i.title), 'children':True}
-                for i in obj.contentValues()]
+        self.positions_table = []
+        for i in obj.contentValues():
+            p_dict = {"id": i.UID(), "text": str(i.title),
+                      'portal_type': i.portal_type}
+            image_url = self.portal_url + '/++resource++baobab.lims.images/'
+            icon_man = '{}{}.png'.format(image_url, i.portal_type.lower())
+            p_dict['icon'] = icon_man
+            if i.portal_type != 'StoragePosition' and \
+                    i.portal_type != 'UnmanagedStorage':
+                p_dict['url'] = i.absolute_url()
+            p_dict['children'] = True
+            if not i.objectValues():
+                p_dict['children'] = False
+
+            self.positions_table.append(p_dict)
         # data['jstree'] = build_tree(hierachy)
-        # self.actions = self.get_workflow_actions()
         # data['actions'] = self.actions
         return json.dumps(self.positions_table)
-
-    def folderitem(self, obj, item, index):
-        if not item.has_key('obj'):
-            return item
-        obj = item['obj']
-        item['Temperature'] = obj.getTemperature()
-        item['Type'] = obj.Type()
-        item['Department'] = obj.getDepartment()and obj.getDepartmentTitle() or ''
-
-        if item['Type'] == 'Unmanaged storage':
-            title = item['id']
-        else:
-            title = item['Title']
-        item['replace']['Title'] = \
-            "<a href='%s'>%s</a>" % (item['url'], title)
-
-        return item
